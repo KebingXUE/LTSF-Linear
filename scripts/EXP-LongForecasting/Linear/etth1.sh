376da5298e060e4951a43f96efc21125f9392533
kxue <kebing.xue@gmail.com>
2023-05-23 19:03:39 +0800

    test

--- scripts/EXP-LongForecasting/Linear/etth1.sh
+++ scripts/EXP-LongForecasting/Linear/etth1.sh
@@ -8,59 +8,19 @@ if [ ! -d "./logs/LongForecasting" ]; then
 fi
 seq_len=336
 model_name=DLinear
-
+for pred_len in 96 192 336 729
+do
 python -u run_longExp.py \
   --is_training 1 \
-  --root_path ./dataset/ \
+  --root_path ~/Work/TSPred/dataset/ETT-small/ \
   --data_path ETTh1.csv \
-  --model_id ETTh1_$seq_len'_'96 \
+  --model_id ETTh1_$seq_len'_'$pred_len \
   --model $model_name \
   --data ETTh1 \
   --features M \
   --seq_len $seq_len \
-  --pred_len 96 \
+  --pred_len $pred_len \
   --enc_in 7 \
   --des 'Exp' \
-  --itr 1 --batch_size 32 --learning_rate 0.005 >logs/LongForecasting/$model_name'_'Etth1_$seq_len'_'96.log
-
-python -u run_longExp.py \
-  --is_training 1 \
-  --root_path ./dataset/ \
-  --data_path ETTh1.csv \
-  --model_id ETTh1_$seq_len'_'192 \
-  --model $model_name \
-  --data ETTh1 \
-  --features M \
-  --seq_len $seq_len \
-  --pred_len 192 \
-  --enc_in 7 \
-  --des 'Exp' \
-  --itr 1 --batch_size 32 --learning_rate 0.005 >logs/LongForecasting/$model_name'_'Etth1_$seq_len'_'192.log
-
-python -u run_longExp.py \
-  --is_training 1 \
-  --root_path ./dataset/ \
-  --data_path ETTh1.csv \
-  --model_id ETTh1_$seq_len'_'336 \
-  --model $model_name \
-  --data ETTh1 \
-  --features M \
-  --seq_len $seq_len \
-  --pred_len 336 \
-  --enc_in 7 \
-  --des 'Exp' \
-  --itr 1 --batch_size 32 --learning_rate 0.005 >logs/LongForecasting/$model_name'_'Etth1_$seq_len'_'336.log
-
-python -u run_longExp.py \
-  --is_training 1 \
-  --root_path ./dataset/ \
-  --data_path ETTh1.csv \
-  --model_id ETTh1_$seq_len'_'720 \
-  --model $model_name \
-  --data ETTh1 \
-  --features M \
-  --seq_len $seq_len \
-  --pred_len 720 \
-  --enc_in 7 \
-  --des 'Exp' \
-  --itr 1 --batch_size 32 --learning_rate 0.005 >logs/LongForecasting/$model_name'_'Etth1_$seq_len'_'720.log
+  --itr 1 --batch_size 32 --learning_rate 0.005 >logs/LongForecasting/$model_name'_'Etth1_$seq_len'_'$pred_len.log
+  done
